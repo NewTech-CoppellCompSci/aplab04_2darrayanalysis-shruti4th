d64d14db8a89770156cd28e3b4294eb1fd0c0915
shruti4th <sjoshi@newtech.coppellisd.com>
2022-10-06 23:57:14 -0500

    finished array analysis (finally!)

--- src/lab/ArrayAnalysis.java
+++ src/lab/ArrayAnalysis.java
@@ -13,14 +13,33 @@ public class ArrayAnalysis {
 	//#1
 	public static int[][] createRandom(int row, int column, int min, int max){
 		
+		int[][] random = new int [row][column]; //create a new array
 		
-		return null;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				int element = (int)(Math.random() * (max - min) + 1);//puts random elements from the range to the array
+				random[i][j] = element;
+			}
+		}
+		
+		return random;
 	}
 	
 	
 	//#2
 	public static void printArray(int[][] arr)  {
 		
+		if(arr == null) {
+			System.out.println("Array is null");
+			return;
+		}
+		
+		for (int[] row : arr) {
+			for (int element: row) {
+				System.out.print(element + " ");
+			}
+			System.out.println();
+		}
 		
 	
 	}
@@ -29,66 +48,153 @@ public class ArrayAnalysis {
 	//#3
 	public static int sumTotal(int[][] arr) {
 		
+		int totalSum = 0;
 		
-		return 0;
+		for (int[] row : arr) {
+			for (int element: row) {
+				totalSum += element;//adds each element to the total sum
+			}
+		}
+		
+		return totalSum;
 	}
 	
 	
 	//#4
 	public static double average(int[][] arr) {
 		
+		int sumForAv = 0;
+		//similar to last one
+		for (int[] row : arr) {
+			for (int element: row) {
+				sumForAv += element;
+				}
+		}
 		
+		int average = sumForAv/arr.length;//divides the sum by the total num of elements in the array
 		
-		return 0;
+		return average;
 	}
 	
 	
 	//#5
 	public static int findMin(int[][] arr) {
 		
+		int curMin = Integer.MAX_VALUE;//sets to highest max value
 		
-		return 0;
+		for(int[]row:arr) {
+			for(int element: row) {
+				curMin = Math.min(element, curMin);//compares using the math method for min
+			}
+		}
+		
+		
+		return curMin;
 	}
 	
 	
 	//#6
 	public static int findMax(int[][] arr) {
 		
+		int curMax = Integer.MIN_VALUE;//sets to lowest min value
 		
-		return 0;
+		for(int[]row:arr) {
+			for(int element: row) {
+				curMax = Math.max(element, curMax);//compares using the math method for max
+			}
+		}
+		
+		return curMax;
 	}
 	
 	
 	//#7
 	public static int[][] diffArrays(int[][] arr1, int[][] arr2){
+		//array for storing difference of each element
 		
 		
-		
-		return null;
+		if(arr1.length == arr2.length) {//checks if num of rows are same
+			int x = arr1.length;
+			
+			if (arr1[0].length == arr2[0].length) {//checks if num columns are same
+				int y = arr1[0].length;
+				
+				int[][] diffArray = new int[x][y];
+			
+				for(int i = 0; i < arr1.length; i++) {
+					for(int j =0; j < arr1[0].length; j++) {
+						diffArray[i][j] = arr1[i][j] - arr2[i][j];//takes the difference of arr1 and arr2
+					}
+				}
+				
+				return diffArray;
+				
+			}  
+			
+		}
+		return null;//returns null if any conditions are false
 	}
 	
 	
 	//#8
 	public static int[][] transposeRow(int[][] arr1, int dist){
 		
+		if (arr1 == null) {
+			return null;
+		}
 		
-		return null;
+		int rows = arr1.length;//number of rows
+		int columns = arr1[0].length;//number of columns
+		int[][] transposedArr = new int[rows][columns];
+		for (int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				int k = i + dist; //k is for keeping track of rows
+				
+				if (k > rows-1) {
+					k = k - rows;//so that its sent back to the top
+				}
+				transposedArr[k][j] = arr1[i][j];
+				
+			}
+		}
+		
+		return transposedArr;
+
 	}
 	
 	
 	//#9
 	public static int sum3D(int[][][] arr) {
+		//similar to 2d array sum
+		int Sum3 = 0;
 		
+		for (int[][] height : arr) {//3 dimensions so has height, row, and element
+			for (int[] row : height) {
+				for(int element:row) {
+					Sum3 += element;
+				}
+			}
+		}
 		
-		return 0;
+
+		return Sum3;
 	}
 	
 	
 	//#10
 	public static double average3D(int[][][] arr) {
+		//similar to 2d array average
+		int sum = 0;
 		
+		for(int [][] height : arr) {
+			for(int [] row : height) {
+				for(int element : row) {
+					sum += element;
+				}
+			}
+		}
 		
-		return 0;
+		return sum/arr.length;//divides the total sum by the num of elements in the array
 	}
 	
 	
--- src/lab/Runner.java
+++ src/lab/Runner.java
@@ -8,16 +8,85 @@ public class Runner {
 		
 		int[][] arr1 = analysis.createRandom(6, 3, -2, 10);
 		
+		int[][]arr2 = analysis.createRandom(4, 3, 0, 90);
 		
+		System.out.print("Array 1: \n");
+		ArrayAnalysis.printArray(arr1);
 		
+		System.out.println();
+		System.out.println();
+
+		System.out.print("Array 2: \n");
+		ArrayAnalysis.printArray(arr2);
 		
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		
+		//#3-6 array 1
 		
+		System.out.println("Array 1 total sum: " + ArrayAnalysis.sumTotal(arr1));
+		System.out.println();
 		
+		System.out.println("Array 1 average: " + ArrayAnalysis.average(arr1));
+		System.out.println();
 		
+		System.out.println("Array 1 lowest value: " + ArrayAnalysis.findMin(arr1));
+		System.out.println();
 		
+		System.out.println("Array 1 highest value: " + ArrayAnalysis.findMax(arr1));
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		
+		//#3-6 array 2
 		
+		System.out.println("Array 2 total sum: " + ArrayAnalysis.sumTotal(arr2));
+		System.out.println();
+		
+		System.out.println("Array 2 average: " + ArrayAnalysis.average(arr2));
+		System.out.println();
+		
+		System.out.println("Array 2 lowest value: " + ArrayAnalysis.findMin(arr2));
+		System.out.println();
+		
+		System.out.println("Array 2 highest value: " + ArrayAnalysis.findMax(arr2));
+		System.out.println();
+		
+		//#7
+		
+		System.out.println("The different between each of the array's values in a new array: \n"); 
+		ArrayAnalysis.printArray(ArrayAnalysis.diffArrays(arr1,  arr2));
+		System.out.println();
+		//#8
+		
+		System.out.println("Transposing Rows: ");
+		ArrayAnalysis.printArray(ArrayAnalysis.transposeRow(arr1, 2));
+		System.out.println();
+		//printing out the 3d array
+		
+		int[][][] arr3D = new int[3][4][5];
+		for (int i = 0; i < arr3D.length; i++) {
+			arr3D[i] = analysis.createRandom(4, 5, -40, 40);
+		}
+		int x = 1;
+		for (int[][] arr : arr3D) {
+			System.out.println("\nLayer #" + x);
+			ArrayAnalysis.printArray(arr);
+			x++;
+		}
+		System.out.println();
+		System.out.println();
+		
+		//#9
+		
+		System.out.println("The sum of the 3D Array: " + ArrayAnalysis.sum3D(arr3D));
+		System.out.println();
+		
+		//#10
+		
+		System.out.println("The average of the 3D array: " + ArrayAnalysis.average3D(arr3D));
+
 	}
 	
 	
